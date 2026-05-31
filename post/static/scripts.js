da9d4d92723d189ed656688e4f0b8d1d55c54282
ASUS <aliasadov475@gmail.com>
2026-05-31 22:00:54 +0400

    Adding Articals page with category filtration

--- post/static/scripts.js
+++ post/static/scripts.js
@@ -12,3 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+const buttons = document.querySelectorAll('.filter-btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        buttons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+
+        button.classList.add('active');
+    });
+});
+
+
--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
+
+
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
@@ -7,3 +9,23 @@ def index(request):
         'posts':posts,
     }
     return render(request, 'index.html',context)
+
+
+def post(request):
+
+    category_id = request.GET.get('category')
+
+    posts = Post.objects.all()
+
+    if category_id:
+        posts = posts.filter(category_id=category_id)
+
+    categories = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'categories': categories,
+        'active_category': category_id
+    }
+
+    return render(request, 'posts.html', context)
